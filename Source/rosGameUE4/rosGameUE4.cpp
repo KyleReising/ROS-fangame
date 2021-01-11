3c886af53ae14a094fa8549d9018627d381b3ea5
Kyle <reisingkyle07@gmail.com>
2021-01-11 12:46:57 -0500

    project remake
    recreated the project using blueprints instead of c++

--- Source/rosGameUE4/rosGameUE4.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "rosGameUE4.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, rosGameUE4, "rosGameUE4" );
